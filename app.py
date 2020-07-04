59c8c7008796e1a59b5a2c18523e369f95581b35
Parikshit22 <parikshitagarwal40@gmail.com>
2020-07-04 08:34:15 +0530

    adding homepage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate,identity
@@ -9,6 +9,10 @@ from resources.store import Store,StoreList
 from db import db
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/')
+def home():
+    return render_template("index1.html")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
