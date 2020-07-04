99ca677b9f21208a5adb8f046562e97e9eb83ea4
Parikshit Agarwal <39821356+Parikshit22@users.noreply.github.com>
2020-07-04 12:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template
 from flask_restful import Api
 from flask_jwt import JWT
+import os
 from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item,ItemList
@@ -12,7 +13,7 @@ api = Api(app)
 @app.route('/')
 def home():
     return render_template("index1.html")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 jwt = JWT(app,authenticate,identity) 
